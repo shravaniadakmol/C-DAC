b9c8e252c2dd5c299644204335afa893fc31c76d
Shravani Adakmol <122961740+shravaniadakmol@users.noreply.github.com>
2026-08-20 01:11:03 +0530

    Add files via upload

---
+++ LBS/Assign/Assign1/Assign1/positive.java
@@ -0,0 +1,11 @@
+class Positive{
+	public static void main (String args[]){
+		int num = 258;
+			if (num > 0) {
+				System.out.println("It's a Positive num");
+			} 
+			else {
+				System.out.println("It's not a Positive num");
+			}
+		}
+}
