7e8e1f369c724737683f51a54711dfec939730bc
Shravani Adakmol <122961740+shravaniadakmol@users.noreply.github.com>
2026-08-21 02:36:40 +0530

    Add files via upload

---
+++ LBS/Assign/Assign1/Area_Rectangle.java
@@ -0,0 +1,9 @@
+class AreaofRectangle{
+	public static void main(String args[]){
+		int L = 6;
+		int H = 2;
+		int area = L * H;
+		System.out.println("Area of Rectangle = " + area);
+		
+	}
+}
---
+++ LBS/Assign/Assign1/EvenOdd.java
@@ -0,0 +1,11 @@
+class EvenOdd {
+	public static void main(String args[]){
+		int num = 25;
+
+		if (num % 2 == 0) {
+			System.out.println("The number is Even");
+		} else {
+			System.out.println("The number is Odd");
+		}
+	}
+}
