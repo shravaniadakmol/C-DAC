36b58e4a5446adf006a893bc99354745735bfc5c
Shravani Adakmol <122961740+shravaniadakmol@users.noreply.github.com>
2026-08-21 02:32:07 +0530

    Add files via upload

---
+++ LBS/Assign/Assign2/DaysoftheWeek.java
@@ -0,0 +1,66 @@
+class DotWeek{
+	public static void main(String args[]){
+		int dayNum1 = 6;
+		int dayNum2 = 9;
+	
+
+		switch (dayNum1) {
+			case 1 : 
+			   System.out.println("The day is Monday");
+				break;
+			case 2 : 
+			   System.out.println("The day is Tueday");
+			   break;
+			case 3 : 
+			   System.out.println("The day is Wednesday");
+		  	   break;
+			case 4 : 
+			   System.out.println("The day is Thursday");
+			   break;
+			case 5 : 
+			   System.out.println("The day is Friday");
+			   break;
+			case 6 : 
+			   System.out.println("The day is Saturday");
+			   break;
+			case 7 : 
+			   System.out.println("The day is Sunday");
+			   break;
+			default:
+			   System.out.println("Invalid day number");
+			   break;
+		}	
+		
+		
+
+		switch (dayNum2) {
+			case 1 : 
+			   System.out.println("The day is Monday");
+				break;
+			case 2 : 
+			   System.out.println("The day is Tueday");
+			   break;
+			case 3 : 
+			   System.out.println("The day is Wednesday");
+		  	   break;
+			case 4 : 
+			   System.out.println("The day is Thursday");
+			   break;
+			case 5 : 
+			   System.out.println("The day is Friday");
+			   break;
+			case 6 : 
+			   System.out.println("The day is Saturday");
+			   break;
+			case 7 : 
+			   System.out.println("The day is Sunday");
+			   break;
+			default:
+			   System.out.println("Invalid day number");
+			   break;
+		}	
+
+
+
+	}
+}
